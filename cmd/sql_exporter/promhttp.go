06e985f69f522d0bd7bf69a3d3cb31c1cfe7ee01
stratomonitor <arnaud_lemaignen@yahoo.com>
2024-12-18 21:46:42 +0100

    Update promhttp.go
    WA for long queries longer than 5 min prometheus lifecycle

--- cmd/sql_exporter/promhttp.go
+++ cmd/sql_exporter/promhttp.go
@@ -126,5 +126,11 @@ func contextFor(req *http.Request, exporter sql_exporter.Exporter) (context.Cont
 	if timeout <= 0 {
 		return context.Background(), func() {}
 	}
+	//#AL
+	// the idea here is that max 5min scrape time from prometheus is not enough for long queries 
+	// so here we override to the configured timeout - timeout offset (40min - 1min for instance)
+	timeout = configTimeout - timeoutOffset
+        klog.Infof("FINAL TIMEOUT (`%s`)", timeout)
+
 	return context.WithTimeout(context.Background(), timeout)
 }
